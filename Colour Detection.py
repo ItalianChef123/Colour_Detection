be8f833daec189620335169addd6f100e075ad26
ItalianChef123 <Ollie-Thornton@hotmail.com>
2022-10-08 20:04:46 +0100

    Organised the functions and added more colour options

--- Colour Detection.py
+++ Colour Detection.py
@@ -8,34 +8,40 @@ def take_frame():
     cam.release()
 
 
-savedOrCamera = input("Do you want to use a saved image or take an image from your webcam ")
-savedOrCamera = savedOrCamera.lower()
-if savedOrCamera == "saved image":
-    image = cv2.imread("image.png")
-elif savedOrCamera == "webcam":
-    take_frame()
-    image = cv2.imread("webcamphoto.png")
-
-
 def click_event(event, x, y, flags, params):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, y)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(image, str(x) + ", " + str(y), (x, y), font, 1, (255, 0, 0), 2)
-        cv2.imshow("image", image)
-    elif event == cv2.EVENT_RBUTTONDOWN:
+    if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
         print(x, ' ', y)
         b = image[y, x, 0]
         g = image[y, x, 1]
         r = image[y, x, 2]
         print(r, g, b)
-        if r <= 255 and g >= 0 and b >= 0 and r >= 100 and g <= 75 and b <= 100:
+        if r <= 255 and g >= 0 and b >= 0 and r >= 65 and g <= 75 and b <= 100:
             print("red")
-        elif r <= 175 and g >= 20 and b >= 0 and r >= 0 and g <= 200 and b <= 200:
+        elif r <= 100 and g >= 0 and b >= 120 and r >= 0 and g <= 100 and b <= 255:
             print("blue")
-        elif  r <= 255 and g >= 130 and b >= 130 and r >= 130 and g <= 255 and b <= 255:
+        elif r <= 255 and g >= 175 and b >= 175 and r >= 175 and g <= 255 and b <= 255:
             print("white")
+        elif r <= 255 and g >= 0 and b >= 50 and r >= 60 and g <= 219 and b <= 255:
+            print("pink")
+        elif r >= 0 and g >= 0 and b >= 0 and r <= 65 and g <= 65 and b <= 65:
+            print("black")
+        elif r >= 0 and g >= 35 and b >= 75 and r <= 165 and g <= 255 and b <= 250:
+            print("turquoise")
+        elif r >= 0 and g >= 60 and b >= 0 and r <= 175 and g <= 255 and b <= 180:
+            print("green")
+        elif r >= 66 and g >= 76 and b >= 0 and r <= 255 and g <= 255 and b <= 150:
+            print("yellow")
+        elif r >= 66 and g >= 76 and b >= 0 and r <= 255 and g <= 255 and b <= 150:
+            print("orange`")
 
+
+savedOrCamera = input("Do you want to use a saved image or take an image from your webcam ")
+savedOrCamera = savedOrCamera.lower()
+if savedOrCamera == "saved image":
+    image = cv2.imread("colours.png")
+elif savedOrCamera == "webcam":
+    take_frame()
+    image = cv2.imread("webcamphoto.png")
 
 image = cv2.resize(image, (1047, 432))
 cv2.imshow("image", image)
